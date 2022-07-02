f165fd7a6f64dd40c41bd120bc9ca684d8aaf47a
viru <viruop1@gmail.com>
2022-07-02 21:09:21 +0530

    first

---
+++ server/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+module.exports = () => {
+    const connectionParams = {
+      useNewUrlParser:true,
+      useUnifiedTopology:true,
+   };
+   try{
+    mongoose.connect(process.env.DB, connectionParams);
+    console.log("connected")
+   }catch(error){
+    console.log(error);
+    console.log("couldn't connect");
+   }
+}
